317a3314271ae48f6127ab8dca086af1cf1af9c2
Fejiro001 <aberefejiro@gmail.com>
2024-11-26 12:41:53 +0100

    Created new components for displaying files and folders

--- src/components/Folder.jsx
+++ src/components/Folder.jsx
@@ -1,35 +1,23 @@
-/* eslint-disable react/prop-types */
-import { useState } from 'react'
+import PropTypes from "prop-types";
+const Folder = ({ item, handleFolderClick, truncateString }) => {
+  return (
+    <div title={item.name} onClick={() => handleFolderClick(item.id)}>
+      <svg
+        className="w-auto h-20 fill-[#12355B]"
+        xmlns="http://www.w3.org/2000/svg"
+        viewBox="0 0 512 512"
+      >
+        <path d="M64 480H448c35.3 0 64-28.7 64-64V160c0-35.3-28.7-64-64-64H288c-10.1 0-19.6-4.7-25.6-12.8L243.2 57.6C231.1 41.5 212.1 32 192 32H64C28.7 32 0 60.7 0 96V416c0 35.3 28.7 64 64 64z" />
+      </svg>
+      <strong className="text-wrap">{truncateString(item.name, 15)}</strong>
+    </div>
+  );
+};
 
-function Folder({ folder, fetchFolderContents }) {
-    const [isOpen, setIsOpen] = useState(false);
+Folder.propTypes = {
+  item: PropTypes.any,
+  handleFolderClick: PropTypes.func.isRequired,
+  truncateString: PropTypes.func.isRequired,
+};
 
-    const handleClick = () => {
-      setIsOpen(!isOpen); // Toggle folder open/close
-      fetchFolderContents(folder.id); // Fetch folder contents when clicked
-    };
-  
-    return (
-      <div>
-        <div onClick={handleClick}>
-          <strong>{folder.name}</strong>
-        </div>
-        {isOpen && folder.subfolders && folder.subfolders.length > 0 && (
-          <div style={{ marginLeft: 20 }}>
-            {folder.subfolders.map((subfolder) => (
-              <Folder key={subfolder.id} folder={subfolder} fetchFolderContents={fetchFolderContents} />
-            ))}
-          </div>
-        )}
-        {isOpen && folder.files && folder.files.length > 0 && (
-          <div style={{ marginLeft: 20 }}>
-            {folder.files.map((file) => (
-              <div key={file.id}>{file.name}</div>
-            ))}
-          </div>
-        )}
-      </div>
-    );
-}
-
-export default Folder
+export default Folder;
