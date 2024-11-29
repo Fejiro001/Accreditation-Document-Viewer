6957db572e134a8190bd9a31e71383e1b19c0c47
Fejiro001 <aberefejiro@gmail.com>
2024-11-29 16:25:12 +0100

    authentication check added to layout

--- src/layout/AuthenticatedLayout.jsx
+++ src/layout/AuthenticatedLayout.jsx
@@ -1,11 +1,37 @@
 import PropTypes from "prop-types";
 import NavBar from "../components/NavBar";
+import { useNavigate } from "react-router-dom";
+import { useEffect, useState } from "react";
+import api from "../api";
 
 function AuthenticatedLayout({ children }) {
+  const navigate = useNavigate();
+  const [userName, setUserName] = useState();
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
+
+  useEffect(() => {
+    const checkAuth = async () => {
+      try {
+        const response = await api.get("/user");
+        setUserName(response.data.name);
+        setIsAuthenticated(true);
+      } catch (error) {
+        console.log(error);
+        navigate("/login", {
+          state: {
+            message: "You must be logged in to view this page.",
+          },
+        });
+      }
+    };
+
+    checkAuth();
+  });
+
   return (
     <div className="flex flex-col min-h-screen items-center bg-slate-100 dark:bg-black">
-      <NavBar />
-      {children}
+      <NavBar userName={userName} />
+      {isAuthenticated && children}
     </div>
   );
 }
