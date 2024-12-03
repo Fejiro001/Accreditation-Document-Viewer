31ec810b72017c064ca049cbb4919db272663267
Fejiro001 <aberefejiro@gmail.com>
2024-12-03 10:15:13 +0100

    new route

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,18 @@
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Login from "./pages/Login";
 import DriveFiles from "./pages/DriveFiles";
+import Users from "./pages/Users";
 
 function App() {
   return (
-    <Router>
-      <Routes>
-        <Route path="/login" element={<Login />} />
-        <Route path="/" element={<DriveFiles />} />
-        <Route path="/folder/:folderId" element={<DriveFiles />} />
-      </Routes>
-    </Router>
+      <Router>
+        <Routes>
+          <Route path="/login" element={<Login />} />
+          <Route path="/" element={<DriveFiles />} />
+          <Route path="/folder/:folderId" element={<DriveFiles />} />
+          <Route path="/admin/users" element={<Users />} />
+        </Routes>
+      </Router>
   );
 }
 
