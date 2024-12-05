b1fcb072b50687e47ba5e8131aa5498047625a0b
Fejiro001 <aberefejiro@gmail.com>
2024-12-05 13:22:33 +0100

    new routes for adding users and users details

--- src/App.jsx
+++ src/App.jsx
@@ -2,17 +2,23 @@ import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Login from "./pages/Login";
 import DriveFiles from "./pages/DriveFiles";
 import Users from "./pages/Users";
+import UserDetails from "./pages/UserDetails";
+import AddUser from "./pages/AddUser";
 
 function App() {
+
   return (
-      <Router>
-        <Routes>
-          <Route path="/login" element={<Login />} />
-          <Route path="/" element={<DriveFiles />} />
-          <Route path="/folder/:folderId" element={<DriveFiles />} />
-          <Route path="/admin/users" element={<Users />} />
-        </Routes>
-      </Router>
+    <Router>
+      <Routes>
+        <Route path="/login" element={<Login />} />
+        <Route path="/" element={<DriveFiles />} />
+        <Route path="/folder/:folderId" element={<DriveFiles />} />
+        {/* Admin Routes */}
+        <Route path="/admin/users" element={<Users />} />
+        <Route path="/admin/users/add" element={<AddUser />} />
+        <Route path="/admin/userDetails" element={<UserDetails />} />
+      </Routes>
+    </Router>
   );
 }
 
