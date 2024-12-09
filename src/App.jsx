05d15a983335df7f33e222176e40807edfad156b
Fejiro001 <aberefejiro@gmail.com>
2024-12-09 14:33:42 +0100

    added edituser route

--- src/App.jsx
+++ src/App.jsx
@@ -2,22 +2,24 @@ import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Login from "./pages/Login";
 import DriveFiles from "./pages/DriveFiles";
 import Users from "./pages/Users";
-import UserDetails from "./pages/UserDetails";
+import EditUser from "./pages/EditUser";
 import AddUser from "./pages/AddUser";
+import { AuthProvider } from "./context/AuthProvider";
 
 function App() {
-
   return (
     <Router>
-      <Routes>
-        <Route path="/login" element={<Login />} />
-        <Route path="/" element={<DriveFiles />} />
-        <Route path="/folder/:folderId" element={<DriveFiles />} />
-        {/* Admin Routes */}
-        <Route path="/admin/users" element={<Users />} />
-        <Route path="/admin/users/add" element={<AddUser />} />
-        <Route path="/admin/userDetails" element={<UserDetails />} />
-      </Routes>
+      <AuthProvider>
+        <Routes>
+          <Route path="/login" element={<Login />} />
+          <Route path="/" element={<DriveFiles />} />
+          <Route path="/folder/:folderId" element={<DriveFiles />} />
+          {/* Admin Routes */}
+          <Route path="/admin/users" element={<Users />} />
+          <Route path="/admin/users/add" element={<AddUser />} />
+          <Route path="/admin/users/:id/edit" element={<EditUser />} />
+        </Routes>
+      </AuthProvider>
     </Router>
   );
 }
