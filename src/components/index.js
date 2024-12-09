43f5cbcbee761be6d3a6a4ceba19690cacdc8340
Fejiro001 <aberefejiro@gmail.com>
2024-12-09 14:39:24 +0100

    index file for barrel exporting from components folder

---
+++ src/components/index.js
@@ -0,0 +1,7 @@
+export { default as FileViewer } from './FileViewer';
+export { default as File } from './File';
+export { default as Folder } from './Folder';
+export { default as SearchBar } from './SearchBar';
+export { default as Loading } from './Loading';
+
+// For barrel exports
